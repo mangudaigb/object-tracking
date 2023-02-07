f039da37ed40a9d6c93ca9c7a17414f5b89036b7
Jibitesh Prasad <jibitesh.prasad@gmail.com>
2023-02-08 00:29:33 +0530

    Added the TrackId and SpanId to outgoing calls too

--- src/main/java/com/gb/trace/interceptor/OutboudTrackerInterceptor.java
+++ src/main/java/com/gb/trace/interceptor/OutboudTrackerInterceptor.java
@@ -1,12 +1,15 @@
 package com.gb.trace.interceptor;
 
+import org.slf4j.MDC;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.*;
 import org.springframework.http.server.ServletServerHttpRequest;
 
 import java.io.IOException;
+import java.net.URI;
 
 
 public class OutboudTrackerInterceptor implements ClientHttpRequestInterceptor {
@@ -14,6 +17,11 @@ public class OutboudTrackerInterceptor implements ClientHttpRequestInterceptor {
 
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-        return null;
+        HttpHeaders headers = request.getHeaders();
+        String traceId = MDC.get("TRACE-ID");
+        String spanId = MDC.get("SPAN-ID");
+        headers.add("TRACE-ID", traceId);
+        headers.add("SPAN-ID", spanId);
+        return execution.execute(request, body);
     }
 }
